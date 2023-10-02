d20bf6c7a2d7c536c1a4ae8b45f134348bf68162
pykto <alan.hdez.olv@gmail.com>
2023-10-02 11:21:50 -0600

    Ceasar cipher and twitter links updated

--- Exercise2.js
+++ Exercise2.js
@@ -2,22 +2,22 @@
 // Write a regular expression that can match:
 
 // Date format <Month-string> <##day>, <####year>
-let dateFormat = /(January|February|March|April|May|June|July|August|September|October|November|December) (\d{2}), (\d{4})/;
+let dateFormat = /^(January|February|March|April|May|June|July|August|September|October|November|December) (\d{2}), (\d{4})$/;
 let dateExamples = ["September 29, 1972","February 99, 0001","June 04, 3000"];
 console.log(dateExamples.every(example => dateFormat.test(example)));
 
 // A letter followed OR preceded by a number
-let letterAndNumber = /\d+[A-Za-z]|[A-Za-z]\d+/;
+let letterAndNumber = /^\d+[A-Za-z]|[A-Za-z]\d+$/;
 let letterAndNumberExamples = ["A52","d747","27X","v2"];
 console.log(letterAndNumberExamples.every(example => letterAndNumber.test(example)));
 
 // txt, java, and cpp files with names consisting of only letters
-let program = /([A-Za-z]+)\.(txt|java|cpp)/;
+let program = /^([A-Za-z]+)\.(txt|java|cpp)$/;
 let programExamples = ["test.java","program.cpp","newReport.txt"];
 console.log(programExamples.every(example => program.test(example)));
 
 // A 5 character palindrome
-let fiveCharPalindrome = /(.)(.).\2\1/;
+let fiveCharPalindrome = /^(.)(.).\2\1$/;
 let fiveCharPalindromeExamples = ["abcba","12321","_1a1_"];
 console.log(fiveCharPalindromeExamples.every(example => fiveCharPalindrome.test(example)));
 
--- Exercise3a.js
+++ Exercise3a.js
@@ -2,39 +2,25 @@
 // Write a program that will:
 
 // Shift cyclically every letter of the alphabet by one, and the numbers as well.
-function shiftChar(str) {
-    if (typeof str != 'string')
-        throw new Error(`Expected string object. Getting ${typeof str} instead`);
-    let format = /(?:([a-zA-z])|(\d+)+)/g;
-    let matches = str.match(format);
-    if (matches.join("") === str) {
-        function shiftLetter(letter) {
-            let code = letter.charCodeAt(); 
-            switch (code) {
-                // char code of "Z" is 90
-                case 90:
-                    // char code of "A" is 65
-                    code = 65;
-                    break;
-                // char code of "z" is 122
-                case 122:
-                    // char Code of "a" is 97
-                    code = 97;
-                    break;
-                default:
-                    code++;
-                    break;
-            }
-            return String.fromCharCode(code);
-        }
-        function shiftNum(num){
-            return String(Number(num) + 1);
-        }
-        return matches.map(match => /\d+/.test(match) ? shiftNum(match) : shiftLetter(match)).join("")
+function ceasarCipher(match, p1,p2,p3, offset, string){
+    if(p1){
+      if (p1.charCodeAt() === 90){
+        return String.fromCharCode(65);
+      }
+      else if (p1.charCodeAt() === 122){
+        return String.fromCharCode(97);
+      }
+      else{
+        return String.fromCharCode(p1.charCodeAt()+1)
+      }
     }
-    else
-        throw new Error("Invalid format of string")
-}
-
-let shiftCharExamples = ["aBc","xyz","aK89"];
-console.log(shiftCharExamples.map(example => shiftChar(example)))
+    if (p2){
+      return String(Number(p2) + 1);
+    }
+    if (p3){
+      return p3
+    }
+  }
+  
+  let shiftCharExamples = ["aBc","xyz","aK89_1"];
+  console.log(shiftCharExamples.map(example => example.replace(/([a-zA-Z])|(\d+)|(.)/gi, ceasarCipher)))
